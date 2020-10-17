13d5ce29b4171d0b9b2da0249db484bf4e695556
ThomasRives <thomas.rives29@gmail.com>
2020-10-17 14:47:53 +0200

    delete struct

--- story.c
+++
@@ -1,6 +0,0 @@
-#include "story.h"
-
-int main(void)
-{
-    return 0;
-}
--- story.h
+++
@@ -1,9 +0,0 @@
-#ifndef STORY_H
-#define STORY_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-/* Déclaration des fonctions que vous faites */
-
-#endif
