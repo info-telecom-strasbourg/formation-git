1dd9ac9003b70898e177045aba138e202581f84e
LyCeline06 <lyceline@hotmail.fr>
2020-10-17 16:22:33 +0200

    add phrase3

--- story.c
+++ story.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+void phrase3(void)
+{
+    printf("et la Tempête l'emporta\n");
+}
+
+
 int main(void)
 {
-    printf("Bienvenue!");
+    phrase3();
     return 0;
 }
